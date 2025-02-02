f3200638f3310ae6a8cdd35a242907e6914c399b
sonjoonseok <joonseok831@gmail.com>
2025-02-03 05:11:31 +0900

    3333

--- lib/common/const/colors.dart
+++ lib/common/const/colors.dart
@@ -8,3 +8,5 @@ const BODY_TEXT_COLOR = Color(0xff868686);
 const INPUT_BG_COLOR = Color(0xfffbfbfb);
 //텍스트필드 테두리 색상
 const INPUT_BORDER_COLOR = Color(0xfff3f2f2);
+//기본 배경 색상
+const MAIN_WINDOW_COLOR = Color(0xff07263b);
